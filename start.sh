5a87d8a05c9839c486e66f93a52fcb1e75a878fc
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-08-01 19:59:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GouthamSER/KuttuBot.git /KuttuBot
+  git clone https://github.com/TheBlackLion17/Wednesday_v1.git /Wednesday_v1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /KuttuBot
+  git clone $UPSTREAM_REPO /Wednesday_v1
 fi
 cd /KuttuBot
 pip3 install -U -r requirements.txt
