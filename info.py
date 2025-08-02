ed3098be902bbb7ec9b11dc615103aa792f3967f
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-08-02 11:26:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ PICS = (environ.get('PICS', 'https://files.catbox.moe/bv36em.jpg https://files.c
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7705748477').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', ' -1002398459249').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002398459249 -1001871354889').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
